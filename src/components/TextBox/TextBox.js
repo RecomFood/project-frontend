cf7b58ba3e2144ce60aa3cf795035c88009eb72d
BHwi <bhwi0422@gmail.com>
2022-10-07 20:05:28 +0900

    mod

--- src/components/TextBox/TextBox.js
+++ src/components/TextBox/TextBox.js
@@ -4,7 +4,7 @@ import "./TextBox.css";
 export default function TextBox({ text, color, placeholder, func }) {
   const add = (e) => {
     console.log(e.key);
-    if (e.key === "Enter") {
+    if (e.key === "Enter" || e.key === "KEYCODE_SEARCH") {
       if (e.nativeEvent.isComposing === false) {
         const value = e.target.value;
 
@@ -26,8 +26,7 @@ export default function TextBox({ text, color, placeholder, func }) {
       <td>
         <input
           className={color}
-          enterkeyhint="search"
-          type="text"
+          type="search"
           placeholder={placeholder}
           onKeyDown={add}
         ></input>
