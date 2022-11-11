dabb1442c21b8313a6ef4a5022cd712bd8549b13
BHwi <bhwi0422@gmail.com>
2022-11-11 09:38:04 +0900

    mod : result router

--- src/Router.js
+++ src/Router.js
@@ -7,7 +7,7 @@ export function Router() {
   return (
     <Routes>
       <Route exact path="/" element={<Main />} />
-      <Route exact path="/result" element={<Result />} />
+      <Route exact path="/result:food" element={<Result />} />
       <Route exact path="/*" element={<Navigate replace to="/" />} />
     </Routes>
   );
